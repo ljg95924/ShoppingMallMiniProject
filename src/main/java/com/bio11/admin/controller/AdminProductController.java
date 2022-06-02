8fb732ea4022d2c30b5f5b51db87867c361976f4
tsseo <tsseo@gmail.com>
2022-06-02 11:47:47 +0900

    guest -> admin 접근제한

--- src/main/java/com/bio11/admin/controller/AdminProductController.java
+++ src/main/java/com/bio11/admin/controller/AdminProductController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,112 +28,128 @@ import java.nio.file.Files;
 @RequestMapping("/admin/product/*")
 @AllArgsConstructor
 public class AdminProductController {
-	
-	private AdminProductService service;
-	
-	@GetMapping("/list")
-	public void list(Model model) {
-		log.info("AdminProductController.list");
-		model.addAttribute("list", service.getProductList());
-	}
 
-	@GetMapping("/register")
-	public void register() {}
+    private AdminProductService service;
 
-	@PostMapping("/register")
-	public String register(ProductDTO product, RedirectAttributes rttr) {
-		log.info("AdminProductController.register");
+    @GetMapping("/list")
+    public String list(Model model, HttpServletRequest request
+            , RedirectAttributes ra) {
+        log.info("AdminProductController.list");
 
-		log.info(product);
-		MultipartFile uploadFile = product.getFile();
+		HttpSession session = request.getSession();
+		session = request.getSession();
+		String adminId = (String) session.getAttribute("adminId");
 
-		String uploadFolder = "/Users/seotaesu/Desimone/FileUpload/";
-		File uploadPath = new File(uploadFolder);
-		log.info("upload Path: " + uploadPath);
-		if(!uploadPath.exists()) {
-			uploadPath.mkdirs();
+		if(adminId == null) {
+			log.info("session : adminId 없음!!");
+			ra.addFlashAttribute("result", "잘못된 접근 입니다.");
+			return "redirect:/product/list?productType=all";
+		}else{
+			model.addAttribute("list", service.getProductList());
+			log.info("session : adminId 존재!!");
+			return "list";
 		}
 
-		String uploadFileName = uploadFile.getOriginalFilename();
-		log.info("uploadFileName: " + uploadFileName);
+    }
 
-		product.setProductImg(uploadFileName);
+    @GetMapping("/register")
+    public void register() {
+    }
 
-		File saveFile = new File(uploadPath, uploadFileName);
-		try {
-			uploadFile.transferTo(saveFile);
-			if (checkImageType(saveFile)) {
-				FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
-				Thumbnailator.createThumbnail(uploadFile.getInputStream(), thumbnail, 70, 70);
-				thumbnail.close();
-			}
-		} catch (IOException e) {
-			log.error(e.getMessage());
-			rttr.addFlashAttribute("status",
-					product.getProductId() + " 상품 등록 실패. 관리자 문의");
-		}
+    @PostMapping("/register")
+    public String register(ProductDTO product, RedirectAttributes rttr) {
+        log.info("AdminProductController.register");
 
-		if (service.registerProduct(product) == 1) {
-			rttr.addFlashAttribute("status",
-					product.getProductId() + " 상품 등록 성공");
-		} else {
-			rttr.addFlashAttribute("status",
-					product.getProductId() + " 상품 등록 실패. 관리자 문의");
-		}
+        log.info(product);
+        MultipartFile uploadFile = product.getFile();
+
+        String uploadFolder = "/Users/seotaesu/Desimone/FileUpload/";
+        File uploadPath = new File(uploadFolder);
+        log.info("upload Path: " + uploadPath);
+        if (!uploadPath.exists()) {
+            uploadPath.mkdirs();
+        }
+
+        String uploadFileName = uploadFile.getOriginalFilename();
+        log.info("uploadFileName: " + uploadFileName);
+
+        product.setProductImg(uploadFileName);
+
+        File saveFile = new File(uploadPath, uploadFileName);
+        try {
+            uploadFile.transferTo(saveFile);
+            if (checkImageType(saveFile)) {
+                FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+                Thumbnailator.createThumbnail(uploadFile.getInputStream(), thumbnail, 70, 70);
+                thumbnail.close();
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            rttr.addFlashAttribute("status",
+                    product.getProductId() + " 상품 등록 실패. 관리자 문의");
+        }
+
+        if (service.registerProduct(product) == 1) {
+            rttr.addFlashAttribute("status",
+                    product.getProductId() + " 상품 등록 성공");
+        } else {
+            rttr.addFlashAttribute("status",
+                    product.getProductId() + " 상품 등록 실패. 관리자 문의");
+        }
 
 
-		return "redirect:/admin/product/list";
-	}
+        return "redirect:/admin/product/list";
+    }
 
-	@GetMapping("detail")
-	public void detail(@RequestParam("productId") int productId, Model model) {
-		log.info("AdminProductController.detail");
-		model.addAttribute("product", service.findProduct(productId));
-	}
+    @GetMapping("detail")
+    public void detail(@RequestParam("productId") int productId, Model model) {
+        log.info("AdminProductController.detail");
+        model.addAttribute("product", service.findProduct(productId));
+    }
 
-	@PostMapping("/modify")
-	public String modify(ProductDTO product, RedirectAttributes rttr) {
-		log.info("AdminProductController.modify");
-		log.info(product);
+    @PostMapping("/modify")
+    public String modify(ProductDTO product, RedirectAttributes rttr) {
+        log.info("AdminProductController.modify");
+        log.info(product);
 
-		if (service.modifyProduct(product) == 1) {
-			rttr.addFlashAttribute("status",
-					product.getProductId() + " 상품 수정 성공");
-		} else {
-			rttr.addFlashAttribute("status",
-					product.getProductId() + " 상품 수정 실패");
-		}
+        if (service.modifyProduct(product) == 1) {
+            rttr.addFlashAttribute("status",
+                    product.getProductId() + " 상품 수정 성공");
+        } else {
+            rttr.addFlashAttribute("status",
+                    product.getProductId() + " 상품 수정 실패");
+        }
 
-		return "redirect:/admin/product/list";
-	}
+        return "redirect:/admin/product/list";
+    }
 
-	@GetMapping("/display")
-	@ResponseBody
-	public ResponseEntity<byte[]> getFile(String fileName) {
-		log.info("fileName: " + fileName);
-		File file = new File("/Users/seotaesu/Desimone/FileUpload/" + fileName);
-		log.info("file : " + file);
+    @GetMapping("/display")
+    @ResponseBody
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        log.info("fileName: " + fileName);
+        File file = new File("/Users/seotaesu/Desimone/FileUpload/" + fileName);
+        log.info("file : " + file);
 
-		ResponseEntity<byte[]> result = null;
-		try {
-			HttpHeaders header = new HttpHeaders();
-			header.add("Content-Type", Files.probeContentType(file.toPath()));
-			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
+        ResponseEntity<byte[]> result = null;
+        try {
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private boolean checkImageType(File file) {
-		try {
-			String contentType = Files.probeContentType(file.toPath());
-			return contentType.startsWith("image");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    private boolean checkImageType(File file) {
+        try {
+            String contentType = Files.probeContentType(file.toPath());
+            return contentType.startsWith("image");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
